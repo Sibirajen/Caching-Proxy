fb18eeb057f81625d07f0d8bdeaea3872ab9d79b
Sibirajen <Sibirajen16@gmail.com>
2025-07-26 14:09:17 +0530

    ProxyServer to start the server

---
+++ src/main/java/com/sibirajen/server/ProxyServer.java
@@ -0,0 +1,23 @@
+package com.sibirajen.server;
+
+import io.undertow.Undertow;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class ProxyServer {
+    private static final String HOST = "localhost";
+
+    public static void start(int port, String origin) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(port, HOST)
+                .setHandler(new ProxyHttpHandler(origin))
+                .build();
+        server.start();
+        log.info("Proxy server started on http://{}:{} → {}", HOST, port, origin);
+        log.info("Listening on http://{}:{}", HOST, port);
+    }
+
+    public static void main(String[] args) {
+        start(8080, "http://dummyjson.com");
+    }
+}
