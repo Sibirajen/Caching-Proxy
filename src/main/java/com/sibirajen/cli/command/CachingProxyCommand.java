dea0f05fcfc8c682aeeaf117cec7e1c745ceec20
Sibirajen <Sibirajen16@gmail.com>
2025-07-26 13:27:17 +0530

    CachingProxyCommand for cli input

---
+++ src/main/java/com/sibirajen/cli/command/CachingProxyCommand.java
@@ -0,0 +1,34 @@
+package com.sibirajen.cli.command;
+
+import com.sibirajen.cli.args.PortValidator;
+import com.sibirajen.cli.args.UrlValidator;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "caching-proxy",
+        version = "Caching Proxy version 1.0",
+        description = "A command-line tool to run a caching proxy server that forwards and caches responses from a target server.",
+        mixinStandardHelpOptions = true
+)
+public class CachingProxyCommand implements Runnable{
+    @CommandLine.Option(
+            names = {"-p", "--port"},
+            description = "Port on which the caching proxy server will run (default: ${DEFAULT-VALUE}).",
+            defaultValue = "9090",
+            preprocessor = PortValidator.class
+    )
+    private int port;
+
+    @CommandLine.Option(
+            names = {"-o", "--origin"},
+            description = "Origin server URL to which requests will be forwarded (e.g., http://example.com).",
+            required = true,
+            preprocessor = UrlValidator.class
+    )
+    private String origin;
+
+    @Override
+    public void run() {
+        System.out.println("Port: " + this.port + ", Origin: " + this.origin);
+    }
+}
