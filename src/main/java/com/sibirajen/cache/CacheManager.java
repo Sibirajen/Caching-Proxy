4122f90f8b9fb1377a585eed46110292feca15ed
Sibirajen <Sibirajen16@gmail.com>
2025-07-29 12:02:30 +0530

    Implemented CacheManager with InMemoryCacheManager

---
+++ src/main/java/com/sibirajen/cache/CacheManager.java
@@ -0,0 +1,4 @@
+package com.sibirajen.cache;
+
+public interface CacheManager<K, V> extends Cache<K, V>{
+}
---
+++ src/main/java/com/sibirajen/cache/InMemoryCacheManager.java
@@ -0,0 +1,25 @@
+package com.sibirajen.cache;
+
+public class InMemoryCacheManager<K, V> implements CacheManager<K, V>{
+    private final Cache<K, V> cache = new LRUCache<>();
+
+    @Override
+    public V get(K key) {
+        return this.cache.get(key);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        this.cache.put(key, value);
+    }
+
+    @Override
+    public void clearCache() {
+        this.cache.clearCache();
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return cache.contains(key);
+    }
+}
